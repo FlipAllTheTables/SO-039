ba97489e06bd7301269f17e1511a891fdc829503
Rafael Ribeiro <rafaelbgwr@gmail.com>
2024-04-12 16:43:49 +0100

    começo de patient.c

--- HOSPITAL/src/patient.c
+++ HOSPITAL/src/patient.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include "patient.h"
+
+int execute_patient(int patient_id, struct data_container* data, struct communication* comm) {
+
+}
+
+void patient_receive_admission(struct admission* ad, int patient_id, struct data_container* data, struct communication* comm) {
+
+}
+
+void patient_process_admission(struct admission* ad, int patient_id, struct data_container* data) {
+
+}
+
+void patient_send_admission(struct admission* ad, struct data_container* data, struct communication* comm) {
+    
+}
