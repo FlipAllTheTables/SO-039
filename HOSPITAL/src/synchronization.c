dcf65a11cbb71651e48503ac53dc4591ba1ff442
Rafael Ribeiro <rafaelbgwr@gmail.com>
2024-05-03 16:39:06 +0100

    ficheiro synchronization.c

---
+++ HOSPITAL/src/synchronization.c
@@ -0,0 +1,14 @@
+/*  Grupo 039
+ * Rafael Ribeiro   - 58193
+ * Pedro Duque      - 52753
+ * Francisco Santos - 59871
+*/ 
+
+#include <stdio.h>
+#include <semaphore.h>
+
+#include "synchronization.h"
+
+sem_t * semaphore_create(char* name, int value) {
+    
+}
