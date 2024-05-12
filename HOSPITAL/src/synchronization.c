b5deaa781a494f66730ea70797255a1037c7e7db
Rafael Ribeiro <rafaelbgwr@gmail.com>
2024-05-12 16:30:46 +0100

    synchronization.c feito

--- HOSPITAL/src/synchronization.c
+++ HOSPITAL/src/synchronization.c
@@ -6,9 +6,61 @@
 
 #include <stdio.h>
 #include <semaphore.h>
+#include <fcntl.h>
 
 #include "synchronization.h"
 
 sem_t * semaphore_create(char* name, int value) {
-    
+    sem_t *sem;
+    sem = sem_open(name, O_CREAT, 0xFFFFFFFF, value);
+    if (sem == SEM_FAILED) {
+        puts("Erro criação de semaphore.");
+        exit(1);
+    }
+    return sem;
+}
+
+void semaphore_destroy(char* name, sem_t* semaphore) {
+    if (sem_close(semaphore) == -1) {
+        puts("Erro sem_close.");
+        exit(1);
+    }
+    if (sem_destroy(name) == -1) {
+        puts("Erro sem_destroy.");
+        exit(1);
+    }
+}
+
+void produce_begin(struct prodcons* pc) {
+    semaphore_lock(pc->empty);
+    semaphore_lock(pc->mutex);
+}
+
+void produce_end(struct prodcons* pc) {
+    semaphore_unlock(pc->mutex);
+    semaphore_unlock(pc->full);
+}
+
+void consume_begin(struct prodcons* pc) {
+    semaphore_lock(pc->full);
+    semaphore_lock(pc->mutex);
+}
+
+void consume_end(struct prodcons* pc) {
+    semaphore_unlock(pc->mutex);
+    semaphore_unlock(pc->empty);
+}
+
+void semaphore_lock(sem_t* sem) {
+    if (sem_wait(sem) == -1) {
+        puts("Erro sem_wait.");
+        exit(1);
+    }
+}
+
+void semaphore_unlock(sem_t* sem) {
+    if (sem_post(sem) == -1) {
+        puts("Erro sem_post.");
+        exit(1);
+    }
 }
